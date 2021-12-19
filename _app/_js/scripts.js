684950d8255fa625dc58c9c575b0595bbcf3e90e
PavelRoR <pavelvp2182@yandex.ru>
2021-12-19 22:57:25 +0500

    Prepearing is done

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,7 +1,60 @@
-//@prepros-prepend jquery-2.1.1.min.js
-
 $(document).ready(function() {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('↙ Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
 
+                // button.text('Отправляем...');
+                // setTimeout(function () {
+                //     button.text('Отправлено!');
+                // }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#aaa",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#aaa",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+    // Модалка
+    $('.button-up').magnificPopup({
+        type: 'inline',
+        midClick: true,
+        mainClass: 'mfp-fade'
+    });
 
 
 /*Конец документа*/
